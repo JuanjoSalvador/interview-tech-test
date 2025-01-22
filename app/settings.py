691f1a3353d586ae060e69c50478facba1752eea
Juanjo Salvador <juanjosalvador@netc.eu>
2025-01-22 12:38:04 +0100

    fix: enviroment variables for develop

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,15 @@
 import dotenv
+import os
 
+ENVIRONMENT: str = os.environ.get('ENVIRONMENT', 'development')
 APP_NAME: str = "MoviesAPI"
 
-FIRESTORE_COLLECTION: str = dotenv.get_key(".env", "FIRESTORE_COLLECTION")
-FIREBASE_WEB_API_KEY: str = dotenv.get_key(".env", "FIREBASE_WEB_API_KEY")
+FIRESTORE_COLLECTION: str = (
+    dotenv.get_key(".env", "FIRESTORE_COLLECTION") if ENVIRONMENT == "development" 
+    else os.environ.get('FIRESTORE_COLLECTION', None)
+)
+
+FIREBASE_WEB_API_KEY: str = (
+    dotenv.get_key(".env", "FIREBASE_WEB_API_KEY") if ENVIRONMENT == "development" 
+    else os.environ.get('FIREBASE_WEB_API_KEY', None)
+)
