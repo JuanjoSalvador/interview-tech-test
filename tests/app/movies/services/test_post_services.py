29ddaf0589689346c8ea80e3ff1f71c05f20205a
Juanjo Salvador <juanjosalvador@netc.eu>
2025-01-22 03:10:22 +0100

    chore: removed non nneded call

--- tests/app/movies/services/test_post_services.py
+++ tests/app/movies/services/test_post_services.py
@@ -29,7 +29,6 @@ def test_create_movie_object(mock_firestore_collection, mock_validate_post):
     assert result == data
 
     mock_validate_post.assert_called_once_with(data)
-    mock_firestore_collection.assert_called_once_with("movies")
     mock_firestore_collection.return_value.document.assert_called_once_with("mock_id")
     mock_query.set.assert_called_once_with(data)
 
