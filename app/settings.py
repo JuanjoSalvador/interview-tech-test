f8933c75cd98028a9a1891eb86562b4a82316637
Juanjo Salvador <juanjosalvador@netc.eu>
2025-01-22 13:23:28 +0100

    chore: reformat settings

--- app/settings.py
+++ app/settings.py
@@ -1,15 +1,17 @@
 import dotenv
 import os
 
-ENVIRONMENT: str = os.environ.get('ENVIRONMENT', 'development')
+ENVIRONMENT: str = os.environ.get("ENVIRONMENT", "development")
 APP_NAME: str = "MoviesAPI"
 
 FIRESTORE_COLLECTION: str = (
-    dotenv.get_key(".env", "FIRESTORE_COLLECTION") if ENVIRONMENT == "development" 
-    else os.environ.get('FIRESTORE_COLLECTION', None)
+    dotenv.get_key(".env", "FIRESTORE_COLLECTION")
+    if ENVIRONMENT == "development"
+    else os.environ.get("FIRESTORE_COLLECTION", None)
 )
 
 FIREBASE_WEB_API_KEY: str = (
-    dotenv.get_key(".env", "FIREBASE_WEB_API_KEY") if ENVIRONMENT == "development" 
-    else os.environ.get('FIREBASE_WEB_API_KEY', None)
+    dotenv.get_key(".env", "FIREBASE_WEB_API_KEY")
+    if ENVIRONMENT == "development"
+    else os.environ.get("FIREBASE_WEB_API_KEY", None)
 )
